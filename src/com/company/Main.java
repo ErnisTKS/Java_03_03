259f5ae82591dab3df392a39b53e44f81f67d9cc
Famy9 <ernis701@gmail.com>
2018-01-31 12:07:20 +0200

    pataisymas

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a=1;
-        while (a!=0){
+        int a=0;
+        int suma=0;
+        do {
             System.out.println("Iveskite skaiciu: ");
             a = sc.nextInt();
-        }
+            suma=suma+a;
+        } while (a!=0);
+
+        System.out.println("Suma: " + suma);
     }
 }
