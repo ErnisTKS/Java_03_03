192f9d49139d0dc048d263dc2f21015d3f43b2c5
Famy9 <ernis701@gmail.com>
2018-01-31 11:46:15 +0200

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a=1;
+        while (a!=0){
+            System.out.println("Iveskite skaiciu: ");
+            a = sc.nextInt();
+        }
+    }
+}
